29e8a1d5445059a0c714a08827c3797bf1a0758d
James Taylor <james@onalog.net>
2025-02-07 08:25:52 +0000

    skeleton project

---
+++ ci/build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_TYPE="${BUILD_TYPE:-Release}"
+BUILD_PROFILE="${BUILD_PROFILE:-linux}"
+BUILD_ROOT="${BUILD_ROOT:-"$(readlink -f "$(dirname "$0")/..")"}"
+BUILD_TYPE_LC="$(echo "$BUILD_TYPE" | tr '[:upper:]' '[:lower:]')"
+BUILD_DIR="${BUILD_DIR:-"cmake-build-${BUILD_TYPE_LC}-${BUILD_PROFILE}"}"
+
+python3 --version
+pip3 --version
+cmake --version
+
+conan profile detect || true
+
+CONAN_SETTINGS=(
+  --profile="$BUILD_ROOT/ci/$BUILD_PROFILE/conan.profile"
+  -s build_type="$BUILD_TYPE"
+  -c tools.cmake.cmake_layout:build_folder_vars="['settings.build_type', 'settings.os']"
+)
+
+cd "$BUILD_ROOT"
+conan install -of "$BUILD_DIR" --build=missing "${CONAN_SETTINGS[@]}" .
+
+source ${BUILD_DIR}/conanbuildenv-${BUILD_TYPE_LC}*.sh
+
+"$CC" --version
+"$CXX" --version
+
+# configure build
+cd "$BUILD_DIR"
+cmake .. \
+  -G "Unix Makefiles" \
+  -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
+  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW \
+  -DBUILD_PROFILE=$BUILD_PROFILE \
+  -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
+
+# put dependencies' dll's on LD_LIBRARY_PATH etc
+source conanrun.sh
+
+# build
+cmake --build . --parallel
+
+# run tests
+ctest .
+
+# package
+cpack -G TGZ .
---
+++ ci/container.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_ROOT="${BUILD_ROOT:-"$(readlink -f "$(dirname "$0")/..")"}"
+BUILD_PROFILE="${BUILD_PROFILE:-linux}"
+BUILD_CONAN_VOLUME=${CONAN_VOLUME:-conan}
+BUILD_ROOT_IN_CONTAINER=/tmp/"$(basename "$BUILD_ROOT")"
+BUILD_IMAGE="${BUILD_IMAGE:-brainfk-"$BUILD_PROFILE"}"
+
+test -d "$BUILD_ROOT"
+test -d "$BUILD_ROOT/ci/$BUILD_PROFILE"
+
+apt update -vy || true
+apt install -vy docker || true
+
+docker build \
+  -t "$BUILD_IMAGE" \
+  -f "$BUILD_ROOT/ci/$BUILD_PROFILE/Dockerfile" \
+  "$BUILD_ROOT/ci"
+
+docker volume create "${BUILD_CONAN_VOLUME}" || true
+
+exec docker run \
+  -i \
+  -v "${BUILD_ROOT}:${BUILD_ROOT_IN_CONTAINER}" \
+  -v "${BUILD_CONAN_VOLUME}:/mnt/conan" \
+  -e CONAN_HOME=/mnt/conan \
+  -e "BUILD_ROOT=${BUILD_ROOT_IN_CONTAINER}" \
+  -e BUILD_TYPE \
+  "$BUILD_IMAGE" \
+  "$@"
